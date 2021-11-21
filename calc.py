ba6328c0eadaf4bf4a76398f91fcd607150b66e4
devanrezqyp <devanrezqy@gmail.com>
2021-11-21 13:30:15 +0700

    first commit

---
+++ calc.py
@@ -0,0 +1,128 @@
+from tkinter import *
+import math
+
+def click(value):
+    ex = entryField.get()  # 789 ex[0:len(ex)-1]
+    answer = ''
+
+    try:
+
+        if value == 'C':
+            ex = ex[0:len(ex) - 1]  # 78
+            entryField.delete(0, END)
+            entryField.insert(0, ex)
+            return
+
+        elif value == 'CE':
+            entryField.delete(0, END)
+
+        elif value == '√':
+            answer = math.sqrt(eval(ex))
+
+        elif value == 'π':
+            answer = math.pi
+
+        elif value == 'cosθ':
+            answer = math.cos(math.radians(eval(ex)))
+
+        elif value == 'tanθ':
+            answer = math.tan(math.radians(eval(ex)))
+
+        elif value == 'sinθ':
+            answer = math.sin(math.radians(eval(ex)))
+
+        elif value == chr(8731):
+            answer = eval(ex) ** (1 / 3)
+
+        elif value == 'x\u02b8':  # 7**2
+            entryField.insert(END, '**')
+            return
+
+        elif value == 'x\u00B3':
+            answer = eval(ex) ** 3
+
+        elif value == 'x\u00B2':
+            answer = eval(ex) ** 2
+
+        elif value == chr(247):  # 7/2=3.5
+            entryField.insert(END, "/")
+            return
+
+        elif value == '=':
+            answer = eval(ex)
+
+        else:
+            entryField.insert(END, value)
+            return
+
+        entryField.delete(0, END)
+        entryField.insert(0, answer)
+
+    except SyntaxError:
+        pass
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+
+def mul(a, b):
+    return a * b
+def div(a, b):
+    return a / b
+
+def mod(a, b):
+    return a % b
+
+def lcm(a,b):
+    l=math.lcm(a,b)
+    return l
+
+def hcf(a,b):
+    h=math.gcd(a,b)
+    return h
+
+operations={'ADD':add,'ADDITION':add,'SUM':add,'PLUS':add,
+            'SUBTRACTION':sub , 'DIFFERENCE':sub , 'MINUS':sub , 'SUBTRACT':sub,
+            'PRODUCT': mul, 'MULTIPLICATION': mul,'MULTIPLY': mul,
+            'DIVISION': div, 'DIV': div, 'DIVIDE': div,
+            'LCM':lcm , 'HCF':hcf,
+            'MOD':mod ,'REMAINDER':mod , 'MODULUS':mod }
+
+
+def findNumbers(t):
+    l=[]
+    for num in t:
+        try:
+            l.append(int(num))
+        except ValueError:
+            pass
+    return l
+
+
+root = Tk()
+root.title('Kalkulator')
+root.config(bg='dodgerblue3')
+root.geometry('550x450')
+
+entryField = Entry(root, font=('arial', 20, 'bold'), bg='dodgerblue3', fg='white', bd=10, relief=SUNKEN, width=30)
+entryField.grid(row=0, column=0, columnspan=6)
+
+button_text_list = ["C", "CE", "√", "+", "π", "cosθ", "tanθ", "sinθ",
+                    "1", "2", "3", "-",
+                    "4", "5", "6", "*", chr(8731), "x\u02b8", "x\u00B3", "x\u00B2",
+                    "7", "8", "9", chr(247),
+                    "0", ".", "%", "="]
+rowvalue = 1
+columnvalue = 0
+for i in button_text_list:
+
+    button = Button(root, width=5, height=2, bd=2, relief=SUNKEN, text=i, bg='dodgerblue3', fg='white',
+                    font=('arial', 18, 'bold'), activebackground='dodgerblue3', command=lambda button=i: click(button))
+    button.grid(row=rowvalue, column=columnvalue, pady=1)
+    columnvalue += 1
+    if columnvalue > 7:
+        rowvalue += 1
+        columnvalue = 0
+
+root.mainloop()
